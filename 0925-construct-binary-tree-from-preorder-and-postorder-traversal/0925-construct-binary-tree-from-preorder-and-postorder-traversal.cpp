3ede34dd4e211784a833f3b22ac4535f6b76fb3d
Priya Sharma <147183123+priyasharanam1@users.noreply.github.com>
2025-02-24 00:21:31 +0530

    Time: 3 ms (39.83%), Space: 28.5 MB (40.55%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -6,32 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        int index = 0;
-        return construct(pre, post, index, 0, pre.size() - 1);
-    }
-    
-private:
-    TreeNode* construct(vector<int>& pre, vector<int>& post, int& index, int l, int h) {
-        if (index >= pre.size() || l > h) 
+    TreeNode* constructTree(vector<int>& pre, vector<int>& post, int& preIndex,
+                            int left, int right,
+                            unordered_map<int, int>& postMap) {
+        if (left > right || preIndex >= pre.size())
             return nullptr;
 
-        TreeNode* root = new TreeNode(pre[index++]);
-        if (l == h) return root;
+        TreeNode* root = new TreeNode(pre[preIndex++]);
+        if (left == right)
+            return root;
 
-        int i = l;
-        while (i <= h && post[i] != pre[index]) i++;
+        int divider =
+            postMap[pre[preIndex]]; // Find left subtree root index in postorder
 
-        if (i <= h) {
-            root->left = construct(pre, post, index, l, i);
-            root->right = construct(pre, post, index, i + 1, h - 1);
-        }
+        root->left = constructTree(pre, post, preIndex, left, divider, postMap);
+        root->right =
+            constructTree(pre, post, preIndex, divider + 1, right - 1, postMap);
 
         return root;
+    }
+
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        int preIndex = 0;
+        unordered_map<int, int> postMap;
+
+        for (int i = 0; i < post.size(); i++)
+            postMap[post[i]] = i;
+
+        return constructTree(pre, post, preIndex, 0, post.size() - 1, postMap);
     }
 };
