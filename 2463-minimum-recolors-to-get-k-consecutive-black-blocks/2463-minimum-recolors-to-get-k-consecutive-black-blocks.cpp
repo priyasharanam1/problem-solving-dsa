fae8daaa246b65710bcec6f23d655a28f86435b3
Priya Sharma <147183123+priyasharanam1@users.noreply.github.com>
2025-03-08 19:18:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (62.68%) - LeetHub

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int n = blocks.length();
+        int minWhite=INT_MAX;
+        for(int i=0;i<n-k+1;i++){
+            int white=0;
+            for(int j=i;j<i+k;j++){
+                if(blocks[j]=='W') white++;
+            }
+            minWhite = min(white,minWhite);
+        }
+        return minWhite;
+    }
+};
