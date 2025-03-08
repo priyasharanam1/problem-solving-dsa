75af66c8ff488ca07d923d2a6e856a59244beddb
Priya Sharma <147183123+priyasharanam1@users.noreply.github.com>
2025-03-08 19:24:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.01%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -2,14 +2,22 @@ class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
         int n = blocks.length();
-        int minWhite=INT_MAX;
-        for(int i=0;i<n-k+1;i++){
-            int white=0;
-            for(int j=i;j<i+k;j++){
-                if(blocks[j]=='W') white++;
-            }
-            minWhite = min(white,minWhite);
+        int white = 0;
+
+        // Count white blocks in the first window
+        for(int i = 0; i < k; i++) {
+            if(blocks[i] == 'W') white++;
         }
+
+        int minWhite = white;
+
+        // Sliding window to find the minimum white blocks in any window of size k
+        for(int i = k; i < n; i++) {
+            if(blocks[i] == 'W') white++; // Add the new block to the window
+            if(blocks[i - k] == 'W') white--; // Remove the leftmost block from the window
+            minWhite = min(minWhite, white);
+        }
+
         return minWhite;
     }
 };
