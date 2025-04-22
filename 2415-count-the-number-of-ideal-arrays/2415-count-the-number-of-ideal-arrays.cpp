b63f3d875a0dad0ddb5a3c229504a4f6e57f0014
Priya Sharma <147183123+priyasharanam1@users.noreply.github.com>
2025-04-22 18:55:07 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.73%) - LeetHub

--- 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.cpp
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.cpp
@@ -1,34 +1,29 @@
-const int mod = 1e9 + 7;
-int factMemo[100000] = {};
-int dp[100000][15];
-using ll = long long;
-class Solution {
-public:
-ll power(ll a, ll b, ll m = mod) {ll res = 1;while (b > 0) {if (b & 1)res = (res * a) % m;a = (a * a) % m;b = b >> 1;}return res;}
-    ll fact(ll n) {
-        if (n == 0) return 1;
-        if (factMemo[n]) return factMemo[n];
-        factMemo[n] = (n * fact(n - 1)) % mod;
-        return factMemo[n];
-    }
-    ll mod_inv(ll a, ll b) {
-        return (((fact(a) * power(fact(b), mod - 2)) % mod) * power(fact(a - b), mod - 2)) % mod;
-    }
+int comb[10001][14] = { 1 }, cnt[10001][14] = {}, mod = 1000000007;
 
-    int idealArrays(int n, int maxi) {
-        for (int i = 1; i <= maxi; i++)
-            for (int j = 1; j <= min(n, 14); j++)
-                dp[i][j] = 0;
-        for (int i = 1; i <= maxi; i++) {
-            dp[i][1] = 1;
-            for (int j = 2; j * i <= maxi; j++)
-                for (int k = 1; k < min(n, 14); k++)
-                    dp[i*j][k+1] += dp[i][k];
+class Solution {
+public: 
+    int idealArrays(int n, int maxValue) {
+        if (comb[1][1] == 0) { // One-time precomputation
+            // Precompute binomial coefficients: comb[s][r] = C(s, r)
+            for (int s = 1; s <= 10000; ++s)
+                for (int r = 0; r < 14; ++r)
+                    comb[s][r] = r == 0 ? 1 : (comb[s - 1][r - 1] + comb[s - 1][r]) % mod;
+
+            // Modified Sieve: cnt[i][bars] = # of sequences ending in i with (bars+1) terms
+            for (int div = 1; div <= 10000; ++div) {
+                ++cnt[div][0];
+                for (int i = 2 * div; i <= 10000; i += div)
+                    for (int bars = 0; cnt[div][bars]; ++bars)
+                        cnt[i][bars + 1] += cnt[div][bars];
+            }
         }
-        ll res = 0;
-        for (int i = 1; i <= maxi; i++)
-            for (int j = 1; j <= min(n, 14); j++)
-                res = (res + mod_inv(n - 1, n - j) * dp[i][j]) % mod;
+
+        int res = 0;
+        // Count valid arrays using precomputed tables
+        for (int i = 1; i <= maxValue; ++i)
+            for (int bars = 0; bars < min(14, n) && cnt[i][bars]; ++bars)
+                res = (1LL * cnt[i][bars] * comb[n - 1][bars] + res) % mod;
+
         return res;
-    } 
+    }
 };
