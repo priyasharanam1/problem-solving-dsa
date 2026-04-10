d7f364a413e77760445296a09822034132b147e2
Priya Sharma <147183123+priyasharanam1@users.noreply.github.com>
2026-04-10 18:16:10 +0530

    Time: 23 ms (23.04%), Space: 8.3 MB (10.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fibo(int n, vector<int>&dp){
         if(n==0 || n==1) return dp[n]=n;
-        if(dp[n]!=-1) return dp[n]=n;
+        if(dp[n]!=-1) return dp[n];
         return fibo(n-1, dp) + fibo(n-2, dp);
     }
 
